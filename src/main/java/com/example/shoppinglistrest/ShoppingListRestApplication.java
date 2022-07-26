4b89a9bd37225b5148f1c315ea97f3cf7ee80877
NeilPraj <neilpraj@gmail.com>
2022-07-25 21:14:56 -0500

    initial commit

---
+++ src/main/java/com/example/shoppinglistrest/ShoppingListRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppinglistrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingListRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppinglistrest/ShoppingListRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppinglistrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
